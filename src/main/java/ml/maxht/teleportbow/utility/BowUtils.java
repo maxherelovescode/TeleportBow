f8faa5d831f15e6f0da1a749acce588f61c19113
Max H <maxplaysbest@gmail.com>
2022-08-05 14:00:01 +1200

    Fixed Colour Codes

--- src/main/java/ml/maxht/teleportbow/utility/BowUtils.java
+++ src/main/java/ml/maxht/teleportbow/utility/BowUtils.java
@@ -15,9 +15,9 @@ public class BowUtils {
 
         ItemStack bow = new ItemStack(Material.BOW, 1);
         ItemMeta bowMeta = bow.getItemMeta();
-        bowMeta.setDisplayName(TeleportBow.getPlugin(TeleportBow.class).getConfig().getString("bowname"));
+        bowMeta.setDisplayName(ChatColor.translateAlternateColorCodes() + TeleportBow.getPlugin(TeleportBow.class).getConfig().getString("bowname"));
         ArrayList lore = new ArrayList<>();
-        lore.add(TeleportBow.getPlugin(TeleportBow.class).getConfig().getString("bowdesc"));
+        lore.add(ChatColor.translateAlternateColorCodes() + TeleportBow.getPlugin(TeleportBow.class).getConfig().getString("bowdesc"));
         bowMeta.setLore(lore);
         bowMeta.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
         bowMeta.addEnchant(Enchantment.DURABILITY, 1000000, true)
